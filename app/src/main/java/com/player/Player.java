792ef4d58e79693d0ee24a38d4d441a3f683535e
Petterluring <petter.gustafsson1998@gmail.com>
2024-11-16 16:24:51 +0100

    Adding methods and fields in players

--- app/src/main/java/com/player/Player.java
+++ app/src/main/java/com/player/Player.java
@@ -1,5 +1,49 @@
 package com.player;
 
+import com.die.Die;
+
+/**
+ * The player class is responsible for representing a player, 
+ * and capture the behaviour of a player such as rolling dice.
+ */
 public class Player {
-    
+    private String name;
+    private static Die[] dice;
+    private static int maxPlayers = 5;
+    private static int amountPlayers = 0;
+
+    public Player(String name) {
+        this.name = name;
+        
+    }
+
+    public void roleDice() {
+
+    }
+
+    public void roleChosenDice(int... dieIDs) {
+
+    }
+
+    public static Die[] getDice() {
+        return dice;
+    }
+
+    public static int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public static void setMaxPlayers(int amount) {
+
+    }
+
+    public static int getAmountPlayers() {
+        return amountPlayers;
+    }
+
+    private static void initDice() {
+        
+    }
+
+
 }
--- app/src/test/java/com/player/PlayerTest.java
+++ app/src/test/java/com/player/PlayerTest.java
@@ -1,5 +1,8 @@
 package com.player;
 
 public class PlayerTest {
+    private final Player player = new Player("Richard");
+
+
     
 }
