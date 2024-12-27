75695d718255255b3139ea5aaa0e310fd0eb5229
Petterluring <petter.gustafsson1998@gmail.com>
2024-12-27 15:18:22 +0100

    Done with scoreTable for now

--- app/src/main/java/com/scoretable/ScoreTable.java
+++ app/src/main/java/com/scoretable/ScoreTable.java
@@ -12,11 +12,31 @@ public class ScoreTable {
     private ScoreColumn[] columns;
 
     public ScoreTable(Player... players) {
-        
+        columns = new ScoreColumn[players.length];
+        for (int i = 0; i < players.length; i++) {
+            Player player = players[i];
+            String firstInitial = String.valueOf(player.getFirstName().charAt(0));
+            String secondInitial = String.valueOf(player.getLastName().charAt(0));
+            columns[i] = new ScoreColumn(player.getID(), firstInitial + secondInitial);
+        }
     }
 
-    public void setPoint() {
-
+    /**
+     * @param playerID - The index of the column array.
+     */
+    public void setPoint(int playerID, String pattern, int point) {
+        ScoreColumn column = getColumn(playerID);
+        column.setPoint(pattern, point);
     }
 
+    public ScoreColumn getColumn(int index) throws IllegalArgumentException {
+        if (index < 0 || index >= columns.length) {
+            throw new IllegalArgumentException("PlayerID does not exist for this table");
+        }
+        return columns[index];
+    }
+
+    public ScoreColumn[] getColumns() {
+        return columns;
+    }
 }
--- app/src/test/java/com/scoretable/ScoreTableTest.java
+++ app/src/test/java/com/scoretable/ScoreTableTest.java
@@ -1,5 +1,40 @@
 package com.scoretable;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.patternnames.PatternNames;
+import com.player.Player;
+import com.scorecolumn.ScoreColumn;
+
 public class ScoreTableTest {
-    
+    private final ScoreTable scoreTable = new ScoreTable(
+        new Player("John", "Doe"), new Player("Joe", "Ericsson"),
+        new Player("Martin", "Ericsson")
+    );
+
+    @Test
+    void canConstructAttributesProperly() {
+        ScoreColumn column = scoreTable.getColumn(0);
+        assertEquals(0, column.getPlayerID());
+        assertEquals("JD", column.getPlayerInitials());
+        column = scoreTable.getColumn(1);
+        assertEquals(1, column.getPlayerID());
+        assertEquals("JE", column.getPlayerInitials());
+        column = scoreTable.getColumn(2);
+        assertEquals(2, column.getPlayerID());
+        assertEquals("ME", column.getPlayerInitials());
+    }
+
+    @Test
+    void canSetPointCorrectly() {
+        scoreTable.setPoint(0, PatternNames.ones, 3);
+        scoreTable.setPoint(1, PatternNames.ones, 3);
+        scoreTable.setPoint(2, PatternNames.ones, 3);
+        for (ScoreColumn column : scoreTable.getColumns()) {
+            assertEquals(3, column.getColumnElement(ScoreColumn.getPatternIndex(PatternNames.ones)));
+        }
+    }
+
 }
